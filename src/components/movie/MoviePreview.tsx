326137ba5ae1b9258eb132105fa89963b9242546
gaboxxz <gaboxxz@gmail.com>
2021-02-17 17:37:06 -0300

    Fixed original title in moview preview. Added script to build ios release

--- src/components/movie/MoviePreview.tsx
+++ src/components/movie/MoviePreview.tsx
@@ -19,7 +19,6 @@ import { AppText, TouchableScale } from '../common';
 const { width } = Dimensions.get('window');
 const previewWidth = width * 0.27;
 export const getMoviePreviewHeight = () => previewWidth / theme.specifications.posterAspectRation;
-var movieTitle;
 /* ------------- Props and State ------------- */
 type OwnProps = {
   movieId?: MovieId;
@@ -56,7 +55,6 @@ class MoviePreview extends React.PureComponent<Props> {
 
   renderMoviePreview() {
     const { movie, highPriority, withRatingBadge } = this.props;
-    movieTitle=movie.original_title;
     if (!movie) return;
     const priority = highPriority ? FastImage.priority.high : FastImage.priority.normal;
     return (
@@ -70,9 +68,9 @@ class MoviePreview extends React.PureComponent<Props> {
   renderEmptyMoviePreview = () => <View style={styles.image} />;
 
   render() {
-    const { movieId } = this.props;
+    const {movie, movieId } = this.props;
     return (
-      <TouchableScale disabled={!movieId} scaleFactor={0.97} style={styles.container} onPress={this.onPress} accessibilityLabel={movieTitle}>
+      <TouchableScale disabled={!movieId} scaleFactor={0.97} style={styles.container} onPress={this.onPress} accessibilityLabel={movie ? movie.original_title : undefined}>
         {movieId ? this.renderMoviePreview() : this.renderEmptyMoviePreview()}
       </TouchableScale>
     );
